99862d7474f613912b2a13e2a27c0ad4d8d73c7e
DaAnMaGi <d.mon26@hotmail.com>
2023-09-11 12:34:57 -0500

    Clase 3 tarea resuelta

--- M3/Clase 03/Notas_clase3_m3.sql
+++ M3/Clase 03/Notas_clase3_m3.sql
@@ -115,5 +115,23 @@ select date(OrderDate),
 	partition by date(OrderDate)
     )
     from salesorderheader;
-    
+   
+-- Las funciones ventanas se realizan sobre una de las vistas o variables a mostrar del QUERY.
+
 -- Otras funciones ventanas: row_number, rank, first_value, last_value, partition by
+
+select * from purchaseorderheader;
+
+select DISTINCT year(OrderDate),
+	sum(TotalDue)
+		over (
+		partition by year(OrderDate)
+		) AS cantidad_compras
+    from purchaseorderheader;
+    
+-- El partition by sirve para "subdividir" los registros que nos encontramos. 
+
+select distinct concat(c.FirstName," ",c.LastName) as "Nombre Cliente",
+	max(h.TotalDue) over (partition by c.ContactID) - min(h.TotalDue) over (partition by c.ContactID) as Diferencia_Gastos
+    from salesorderheader h
+    join contact c	on (c.ContactID = h.ContactID);
