7e09cf3b7024b45a5569686e286ae51a2dd3414d
DaAnMaGi <d.mon26@hotmail.com>
2023-09-12 22:25:46 -0500

    revision punto 3

--- M3/Clase 04/comprobar_tablas_clase4.sql
+++ M3/Clase 04/comprobar_tablas_clase4.sql
@@ -15,3 +15,14 @@ select * from proveedores;
 select * from sucursales;
 select * from tipogasto;
 select * from venta;
+
+drop table if exists canalventa;
+drop table if exists clientes;
+drop table if exists compra;
+drop table if exists empleados;
+drop table if exists gasto;
+drop table if exists productos;
+drop table if exists proveedores;
+drop table if exists sucursales;
+drop table if exists tipogasto;
+drop table if exists venta;
